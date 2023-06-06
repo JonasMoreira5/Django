da992bcd1e623d987c1fb773b0638217cf836d29
JonasMoreira5 <jonasmoreira076@gmail.com>
2023-06-05 22:07:09 -0400

    Readme

--- website/forms.py
+++ website/forms.py
@@ -1,19 +1,1 @@
 from django import forms
-
-class InsereFuncioanarioFor(forms.Form)
-
-    nome = forms.CharField(
-        required=True,
-        max_length=255
-    )
-    
-    sobrenome = forms.CharField(
-        required=True,
-        max_length=255
-    )
-    
-
-    cpf = forms.CharField(
-        required=True,
-        max_length=14
-    )
